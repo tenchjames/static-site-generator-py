9b3e9676c7375b8e3d88027855dd50580f6f75fc
James Tench <tenchjames@gmail.com>
2024-12-17 22:46:45 -0500

    initial commit, added TextNode and project setup

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode("text in the node", TextType.BOLD)
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
